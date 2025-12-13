db5d97262d4c84803f2bff43fc88cfcf3680622e
Alastair Locke <lockealastair@proton.me>
2025-12-13 16:06:54 +1100

    Updates

--- create_simlinks.sh
+++ create_simlinks.sh
@@ -1,6 +1,20 @@
 
 dotfiles_home=~/hyprland-dotfiles-al/home
 
-# cp -rsf "$dotfiles_home"//. ~
-# ln -sT "$dotfiles_home"/.config/hypr ~/test
+
+# Desktop
 ln -sT "$dotfiles_home"/.config/hypr ~/.config/hypr
+ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
+
+
+# Terminal
+ln -sT "$dotfiles_home"/.config/alacritty ~/.config/alacritty
+ln -sT "$dotfiles_home"/.config/bash ~/.config/bash
+ln -sT "$dotfiles_home"/.config/zsh ~/.config/zsh
+ln -sT "$dotfiles_home"/.config/shell.env ~/.config/shell.env
+ln -sT "$dotfiles_home"/.config/starship ~/.config/starship
+
+
+
+# Low risk
+ln -sT "$dotfiles_home"/.config/fastfetch ~/.config/fastfetch
