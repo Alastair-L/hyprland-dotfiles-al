a21772feeebfe302248c8cdc2b1042b4f37a8d35
Alastair Locke <lockealastair@proton.me>
2025-12-13 23:42:38 +1100

    Paste in hypr from original

--- create_simlinks.sh
+++ create_simlinks.sh
@@ -3,29 +3,45 @@ dotfiles_home=~/hyprland-dotfiles-al/home
 
 
 # Desktop
-ln -sT "$dotfiles_home"/.config/hypr ~/.config/hypr
-ln -sT "$dotfiles_home"/.config/scripts ~/.config/scripts
-ln -sT "$dotfiles_home"/.config/swaync ~/.config/swaync
-ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
-ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
+# ln -sT "$dotfiles_home"/.config/hypr ~/.config/hypr
+# ln -sT "$dotfiles_home"/.config/scripts ~/.config/scripts
+# ln -sT "$dotfiles_home"/.config/swaync ~/.config/swaync
+# ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
 
 
 ln -sT "$dotfiles_home"/.config/bat ~/.config/bat
 ln -sT "$dotfiles_home"/.config/doom ~/.config/doom
 ln -sT "$dotfiles_home"/.config/dunst ~/.config/dunst
+ln -sT "$dotfiles_home"/.config/fastfetch ~/.config/fastfetch
+ln -sT "$dotfiles_home"/.config/ghostty ~/.config/ghostty
+ln -sT "$dotfiles_home"/.config/git ~/.config/git
+ln -sT "$dotfiles_home"/.config/helix ~/.config/helix
+ln -sT "$dotfiles_home"/.config/hypr ~/.config/hypr
+ln -sT "$dotfiles_home"/.config/hyprpanel ~/.config/hyprpanel
+ln -sT "$dotfiles_home"/.config/kanata ~/.config/kanata
+ln -sT "$dotfiles_home"/.config/kitty ~/.config/kitty
+ln -sT "$dotfiles_home"/.config/mpv ~/.config/mpv
+ln -sT "$dotfiles_home"/.config/nvim ~/.config/nvim
+ln -sT "$dotfiles_home"/.config/playerctl ~/.config/playerctl
+ln -sT "$dotfiles_home"/.config/posting ~/.config/posting
+ln -sT "$dotfiles_home"/.config/qBittorrent ~/.config/qBittorrent
+ln -sT "$dotfiles_home"/.config/qutebrowser ~/.config/qutebrowser
+ln -sT "$dotfiles_home"/.config/rofi ~/.config/rofi
+ln -sT "$dotfiles_home"/.config/screenshots ~/.config/screenshots
+ln -sT "$dotfiles_home"/.config/scripts ~/.config/scripts
+ln -sT "$dotfiles_home"/.config/tmux ~/.config/tmux
 ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
-ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
-ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
-ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
-ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
-ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
+ln -sT "$dotfiles_home"/.config/waypaper ~/.config/waypaper
+ln -sT "$dotfiles_home"/.config/yazi ~/.config/yazi
+ln -sT "$dotfiles_home"/.config/zathura ~/.config/zathura
+ln -sT "$dotfiles_home"/.config/zed ~/.config/zed
 
 
 # Terminal
-ln -sT "$dotfiles_home"/.config/alacritty ~/.config/alacritty
-ln -sT "$dotfiles_home"/.config/bash ~/.config/bash
-ln -sT "$dotfiles_home"/.config/zsh ~/.config/zsh
-ln -sT "$dotfiles_home"/.config/shell.env ~/.config/shell.env
-ln -sT "$dotfiles_home"/.config/starship ~/.config/starship
+# ln -sT "$dotfiles_home"/.config/alacritty ~/.config/alacritty
+# ln -sT "$dotfiles_home"/.config/bash ~/.config/bash
+# ln -sT "$dotfiles_home"/.config/zsh ~/.config/zsh
+# ln -sT "$dotfiles_home"/.config/shell.env ~/.config/shell.env
+# ln -sT "$dotfiles_home"/.config/starship ~/.config/starship
 
 
