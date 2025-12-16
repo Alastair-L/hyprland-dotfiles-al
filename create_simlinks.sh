eb84b84250b88f566f077f1ec71b693a8bc27bfa
Alastair Locke <lockealastair@proton.me>
2025-12-16 21:43:41 +1100

    Unprogress

--- create_simlinks.sh
+++ create_simlinks.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 
-dotfiles_home=$HOME/hyprland-dotfiles-al/home
+dotfiles_home="$HOME/hyprland-dotfiles-al/home"
 
-
-configs=(
+all_configs=(
   bat
+  bash
   doom
   dunst
   fastfetch
@@ -31,50 +32,45 @@ configs=(
   zed
 )
 
-for cfg in "${configs[@]}"; do
-  ln -sT "$dotfiles_home/.config/$cfg" "$HOME/.config/$cfg"
+
+current_configs=(
+  hypr
+  rofi
+  scripts
+  waybar
+  qutebrowser
+)
+
+# mapfile -t delete_configs < <(
+#   comm -23 <(echo "${all_configs[@]}" | tr ' ' '\n' | sort) \
+#            <(echo "${current_configs[@]}" | tr ' ' '\n' | sort)
+# )
+
+
+#######################################
+# 1. Clear out configs iff they are symlinks
+#######################################
+for cfg in "${all_configs[@]}"; do
+  target="$HOME/.config/$cfg"
+
+  if [[ -L "$target" ]]; then
+    echo "Removing symlink: $target"
+    rm "$target"
+  fi
 done
 
-# Desktop
-# ln -sT "$dotfiles_home"/.config/hypr ~/.config/hypr
-# ln -sT "$dotfiles_home"/.config/scripts ~/.config/scripts
-# ln -sT "$dotfiles_home"/.config/swaync ~/.config/swaync
-# ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
+#######################################
+# 2. Recreate symlinks from dotfiles
+#######################################
+for cfg in "${current_configs[@]}"; do
+  src="$dotfiles_home/.config/$cfg"
+  dst="$HOME/.config/$cfg"
 
-
-ln -sT "$dotfiles_home"/.config/bat ~/.config/bat
-ln -sT "$dotfiles_home"/.config/doom ~/.config/doom
-ln -sT "$dotfiles_home"/.config/dunst ~/.config/dunst
-ln -sT "$dotfiles_home"/.config/fastfetch ~/.config/fastfetch
-ln -sT "$dotfiles_home"/.config/ghostty ~/.config/ghostty
-ln -sT "$dotfiles_home"/.config/git ~/.config/git
-ln -sT "$dotfiles_home"/.config/helix ~/.config/helix
-ln -sT "$dotfiles_home"/.config/hypr ~/.config/hypr
-ln -sT "$dotfiles_home"/.config/hyprpanel ~/.config/hyprpanel
-ln -sT "$dotfiles_home"/.config/kanata ~/.config/kanata
-ln -sT "$dotfiles_home"/.config/kitty ~/.config/kitty
-ln -sT "$dotfiles_home"/.config/mpv ~/.config/mpv
-ln -sT "$dotfiles_home"/.config/nvim ~/.config/nvim
-ln -sT "$dotfiles_home"/.config/playerctl ~/.config/playerctl
-ln -sT "$dotfiles_home"/.config/posting ~/.config/posting
-ln -sT "$dotfiles_home"/.config/qBittorrent ~/.config/qBittorrent
-ln -sT "$dotfiles_home"/.config/qutebrowser ~/.config/qutebrowser
-ln -sT "$dotfiles_home"/.config/rofi ~/.config/rofi
-ln -sT "$dotfiles_home"/.config/screenshots ~/.config/screenshots
-ln -sT "$dotfiles_home"/.config/scripts ~/.config/scripts
-ln -sT "$dotfiles_home"/.config/tmux ~/.config/tmux
-ln -sT "$dotfiles_home"/.config/waybar ~/.config/waybar
-ln -sT "$dotfiles_home"/.config/waypaper ~/.config/waypaper
-ln -sT "$dotfiles_home"/.config/yazi ~/.config/yazi
-ln -sT "$dotfiles_home"/.config/zathura ~/.config/zathura
-ln -sT "$dotfiles_home"/.config/zed ~/.config/zed
-
-
-# Terminal
-# ln -sT "$dotfiles_home"/.config/alacritty ~/.config/alacritty
-# ln -sT "$dotfiles_home"/.config/bash ~/.config/bash
-# ln -sT "$dotfiles_home"/.config/zsh ~/.config/zsh
-# ln -sT "$dotfiles_home"/.config/shell.env ~/.config/shell.env
-# ln -sT "$dotfiles_home"/.config/starship ~/.config/starship
-
-
+  if [[ ! -e "$src" ]]; then
+    echo "Warning: source does not exist, skipping: $src"
+    continue
+  fi
+  
+  echo "Linking: $dst → $src"
+  ln -sT "$src" "$dst"
+done
